2d8434ab85dcd208bc0d50d55ec50f2c77ebac1a
koyeo <im@koyeo.com>
2024-07-12 17:15:18 +0800

    feat: test twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,17 @@ export default function RootLayout({
                                    }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const url = 'https://baidu.com'; // 点击 twitter 卡片时跳转的地址
+    const image = 'http://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/6c85f43aed61e320ebec194e6a78d6d3.png~tplv-uwbnlip3yd-png.png'; // 卡片识别到的图片地址
     return (
         <html lang="en">
         <body className={styles.body}>{children}</body>
+        <meta name="twitter:site" content="@xxx"/>
+        <meta name="twitter:creator" content="@xxx"/>
+        <meta property="og:url" content={url}/>
+        <meta property="og:title" content="memescan:title"/>
+        <meta property="og:image" content={image}/>
+        <meta content="summary_large_image" name="twitter:card"/>
         </html>
     );
 }
